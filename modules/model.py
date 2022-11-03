380fbda1ea182b2b0075e982046b0b0aca50ad92
jonojace <jonojace@gmail.com>
2022-11-03 07:50:52 +0000

    add some code for respeller

--- modules/model.py
+++ modules/model.py
@@ -2,19 +2,96 @@ import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from torch.nn import TransformerEncoder, TransformerEncoderLayer, LayerNorm
+from gumbel_vector_quantizer import GumbelVectorQuantizer
 
-class Respeller(nn.Module):
-    def __init__(self):
+# class EncoderDecoderRespeller(nn.Module):
+#     def __init__(self):
+#         super().__init__()
+#         self.encoder =
+#         self.decoder =
+
+class EncoderRespeller(nn.Module):
+    def __init__(
+            self,
+            vocab_size,
+            embedding_dim=128,
+            d_model=512,
+            nhead=4,
+            num_layers=4,
+            pretrained_embedding_table=None, # optional - initialise from pretrained grapheme embedding table from TTS
+            freeze_embedding_table=False,
+    ):
         super().__init__()
+        self.model_type = 'EncoderRespeller'
+        self.embedding = nn.Embedding(vocab_size, embedding_dim)
 
-        self.encoder
+        if pretrained_embedding_table is not None:
+            initialise_embedding_table(self.embedding, pretrained_embedding_table)
+            if freeze_embedding_table:
+                # freeze_embedding_table.freeze_weights()
+                raise NotImplementedError
 
-        self.decoder
-
-        self.quantiser
+        self.pos_encoder = PositionalEncoding(embedding_dim)
+        encoder_layer = TransformerEncoderLayer(
+            d_model,
+            nhead,
+            batch_first=True,
+        )
+        encoder_norm = LayerNorm(d_model)
+        self.encoder = TransformerEncoder(
+            encoder_layer,
+            num_layers,
+            norm=encoder_norm,
+        )
 
     def forward(self, inputs):
-        encodings = self.encoder(inputs)
-        logits = self.decoder(encodings)
-        respelling = self.quantiser(logits)
-        return respelling
+        inputs = self.embedding(inputs)
+        inputs = self.pos_encoder(inputs)
+        logits = self.encoder(inputs)
+        return logits
+
+class PositionalEncoding(nn.Module):
+    r"""Inject some information about the relative or absolute position of the tokens in the sequence.
+        The positional encodings have the same dimension as the embeddings, so that the two can be summed.
+        Here, we use sine and cosine functions of different frequencies.
+    .. math:
+        \text{PosEncoder}(pos, 2i) = sin(pos/10000^(2i/d_model))
+        \text{PosEncoder}(pos, 2i+1) = cos(pos/10000^(2i/d_model))
+        \text{where pos is the word position and i is the embed idx)
+    Args:
+        d_model: the embed dim (required).
+        dropout: the dropout value (default=0.1).
+        max_len: the max. length of the incoming sequence (default=5000).
+    Examples:
+        >>> pos_encoder = PositionalEncoding(d_model)
+    """
+
+    def __init__(self, d_model, dropout=0.1, max_len=5000):
+        super(PositionalEncoding, self).__init__()
+        self.dropout = nn.Dropout(p=dropout)
+
+        pe = torch.zeros(max_len, d_model)
+        position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model))
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+        pe = pe.unsqueeze(0).transpose(0, 1)
+        self.register_buffer('pe', pe)
+
+    def forward(self, x):
+        r"""Inputs of forward function
+        Args:
+            x: the sequence fed to the positional encoder model (required).
+        Shape:
+            x: [sequence length, batch size, embed dim]
+            output: [sequence length, batch size, embed dim]
+        Examples:
+            >>> output = pos_encoder(x)
+        """
+
+        x = x + self.pe[:x.size(0), :]
+        return self.dropout(x)
+
+def initialise_embedding_table(nn_embedding, weights):
+    raise NotImplementedError
--- train.py
+++ train.py
@@ -9,21 +9,65 @@ We can backpropagate through these using gumbel softmax and the straight through
 '''
 
 from modules.model import Respeller
+from modules.gumbel_vector_quantizer import GumbelVectorQuantizer
 
 def train():
-    # models
-    tts = Fastpitch()
-    tts.freeze weights()
-    respeller = Respeller(out_dim=len(tts.input_symbols), audio=use_audio, quantisation='gumbel_softmax')
+    tts = load_fastpitch()
+    tts = convert_embedding_table_to_linear_layer(tts)
+    tts.freeze_weights()
 
-    # forward pass
-    respelling = respeller(text, audio=ssl_reps)
-    log_mel = tts(respelling)
-    loss = l2(log_mel, gt_log_mel)
+    if 'modeltype' == 'autoregressive':
+        respeller = EncoderDecoderRespeller(out_dim=len(tts.input_symbols))
+    elif 'modeltype' == 'non_autoregressive':
+        respeller = EncoderRespeller(out_dim=len(tts.input_symbols))
 
-    # backward pass
-    loss.backward()
+    quantiser = GumbelVectorQuantizer(
+        dim=self.decoder.outdim,
+        num_vars=cfg.latent_vars,  # 320 - number of latent variables V in each group of the codebook
+        temp=cfg.latent_temp, # (2, 0.5, 0.999995) - temperature for latent variable sampling. can be tuple of 3 values (start, end, decay)
+        groups=cfg.latent_groups,  # 2 - number of groups G of latent variables in the codebook
+        combine_groups=False,
+        vq_dim=vq_dim,
+        time_first=True,
+        weight_proj_depth=cfg.quantizer_depth,
+        weight_proj_factor=cfg.quantizer_factor,
+    )
 
-    # log tensorboard metrics
+    acoustic_loss_fn = Softdtw()
 
-    # validation set evaluation
+    # if args.respeller_loss:
+    #     respelling_loss_fn = CrossEntropy()
+    # else:
+
+    for batch in batches:
+        ###############################################################################################################
+        # text, ssl_reps, e2e_asr_predictions, gt_log_mel = batch
+        text, gt_log_mel = batch
+
+        ###############################################################################################################
+        # create inputs
+        inputs = text
+        # if args.use_acoustic_input:
+        #     inputs = inputs.concat(ssl_reps)
+
+        ###############################################################################################################
+        # forward pass
+        logits = respeller(inputs)
+        respelling = quantiser(logits)
+        log_mel = tts(respelling)
+
+        ###############################################################################################################
+        # calculate losses
+        # respelling_loss = respelling_loss_fn(respelling, e2e_asr_predictions)
+        acoustic_loss = acoustic_loss_fn(log_mel, gt_log_mel)
+
+        ###############################################################################################################
+        # backward pass
+        loss = acoustic_loss
+        loss.backward()
+
+        ###############################################################################################################
+        # log tensorboard metrics
+
+        ###############################################################################################################
+        # validation set evaluation
